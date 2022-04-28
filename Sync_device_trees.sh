d3df8b9d762811f7f2b0d14f7d4873d4f49b278b
Meghthedev <meghthedev@gmail.com>
2022-04-28 12:38:39 +0530

    Whole Rewrite of the script to include a selection of 32bit or 64bit trees

--- Sync_device_trees.sh
+++ Sync_device_trees.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 #script to sync repos for oppo A37
 #run this in your rom root dir
-cd device
-mkdir oppo && cd oppo
-git clone https://github.com/DeepakChaurasia30/rb_device_oppo_A37.git A37
-cd ..
-cd ..
-cd vendor
-mkdir oppo && cd oppo
-git clone https://github.com/DeepakChaurasia30/rb-vendor_oppo.git
-#make sure to edit this script and change your rom path
-cd rb-vendor_oppo && mv A37 /mnt/500/android/carbon/vendor/oppo/ && mv .git/ /mnt/500/android/carbon/vendor/oppo/ && cd .. && rm -rf rb-vendor_oppo 
-cd ..
-cd ..
-cd kernel
-mkdir oppo && cd oppo
-git clone https://github.com/rakeshbhagat5615/kernel_oppo_msm8939.git msm8939 
-cd ..
+echo This is a script to sync trees for oppo A37 
+
+Architecture=$1
+
+echo Please give your selection Of Architecture as 32bit rakeshbhagat5615 or 64bit DeepakChaurasia30
+read Architecture
+echo Syncing Trees!
+
+if [ $1=32 ]; then
+    git clone https://github.com/DeepakChaurasia30/rb_device_oppo_A37.git device/oppo/A37
+    cd vendor 
+    git clone https://github.com/DeepakChaurasia30/rb-vendor_oppo.git oppo
+    cd ../..
+    git clone https://github.com/rakeshbhagat5615/kernel_oppo_msm8939.git kernel/oppo/msm8939 
+fi 
+
+elif [ $1=64 ] then 
+    git clone https://github.com/DeepakChaurasia30/android-device_oppo_A37.git device/oppo/A37
+    git clone https://github.com/DeepakChaurasia30/-android_device_cyanogen_msm8916-common.git device/cyanogen/msm8916
+    git clone https://github.com/DeepakChaurasia30/kernel_oppo_msm8939.git kernel/oppo/msm8939
+    cd vendor 
+    git clone https://github.com/DeepakChaurasia30/rb-vendor_oppo.git oppo
+    cd ../..
+fi
+
